923798f119bdee77684f24e74e348b8866d333c3
German Zvezdin <germanzvezdin@yandex.ru>
2020-10-16 22:08:28 +0300

    change cycle

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "z-curve-arrays.hpp"
 #include <iostream>
-
+#include <time.h>
 int main() {
     int N = 100;
     double a = 3.0;
     double h = 0.2;
-    double tau = pow(h, 2) / a;
+    double tau = pow(h, 3) / a;
     ArrD<double, 2> u, v;
     u.init(ind(N,N));
     v.init(ind(N,N));
@@ -15,10 +15,11 @@ int main() {
         }
     }
     u[ind(N / 2,N / 2)] = 1.0;
+    double t1 = clock();
     double c = a * (tau) / (h * h);
     for(int t = 0; t < 100; t++){
-        for(int i = 1; i < N - 1; i++){
-            for(int j = 1; j < N - 1; j++){
+        for(int j = 1; j < N - 1; j++){
+            for(int i = 1; i < N - 1; i++){
                 v[ind(i, j)] = (c * (u[ind(i + 1, j)] + u[ind(i, j + 1)]+
                                u[ind(i - 1, j)] + u[ind(i, j - 1)] -
                                4 * u[ind(i, j)]) + u[ind(i, j)]);
@@ -26,6 +27,9 @@ int main() {
         }
         u = v;
     }
+    //0.391279 i -> j
+    // 0.334201 j -> i
+    std::cout <<"TIME: "<< (double)(clock() - t1)/CLOCKS_PER_SEC <<std::endl;
     FILE *x, *y, *z;
     x = fopen("x.txt", "w");
     y = fopen("y.txt", "w");
