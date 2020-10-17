029567cb7ed5f860c50c279c57f9d141b83c26af
German Zvezdin <germanzvezdin@yandex.ru>
2020-10-17 22:01:02 +0300

    add first task

---
+++ ordinary_differential_equations/cmake-build-debug/CMakeFiles/ordinary_differential_equations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ordinary_differential_equations.dir/main.cpp.o"
+  "ordinary_differential_equations"
+  "ordinary_differential_equations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ordinary_differential_equations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
